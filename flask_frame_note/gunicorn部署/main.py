e747312041c9893b51e092ba6a9d3e472e6e78df
dcdmm <dcdmmcomeon@gmail.com>
2023-02-23 17:36:17 +0800

    增强理解

--- flask_frame_note/gunicorn部署/main.py
+++ flask_frame_note/gunicorn部署/main.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return 'hello world'
 
+'''
+gunicorn介绍:
+What is WSGI?
+WSGI is the Web Server Gateway Interface. It is a specification that describes how a web server communicates with web applications, and how web applications can be chained together to process one request.
+
+WSGI is a Python standard described in detail in PEP 3333(https://www.python.org/dev/peps/pep-3333).
+
+For more, see Learn about WSGI(https://www.python.org/dev/peps/pep-3333).
+
+Gunicorn 'Green Unicorn' is a Python WSGI HTTP Server for UNIX. It's a pre-fork worker model. 
+The Gunicorn server is broadly compatible with various web frameworks, simply implemented, light on server resources, and fairly speedy.
+'''
 # gunicorn部署flask:
 # Linux:gunicorn -c conf/gunicorn.py main:app
 # docker:CMD ["gunicorn", "-c", "conf/gunicorn.py", "main:app"]
