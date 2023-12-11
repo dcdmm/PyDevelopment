bf69e24bd65707da7c15c055a185f32a6ce21e20
dcdmm <dcdmmcomeon@gmail.com>
2023-12-11 11:17:14 +0800

    进一步理解

--- setuptools_note/setuptools_base/setup.py
+++ setuptools_note/setuptools_base/setup.py
@@ -3,18 +3,35 @@ from setuptools import setup, find_packages
 
 extras_require = {}
 
-package_name = "mypackage"
+'''
+作用:
+pip list 
+显示如下:
+xyzqwer                   0.0.1
+
+pip uninstall xyzqwer
+显示如下:
+Found existing installation: xyzqwer 0.0.1
+Uninstalling xyzqwer-0.0.1:
+  Would remove:
+    c:\users\duanm\anaconda3\envs\development_env\lib\site-packages\mypackage1\*
+    c:\users\duanm\anaconda3\envs\development_env\lib\site-packages\mypackage\*
+    c:\users\duanm\anaconda3\envs\development_env\lib\site-packages\xyzqwer-0.0.1.dist-info\*
+Proceed (Y/n)?
+
+'''
+package_name = "xyzqwer"
 
 # 分析:
 # 1. find_packages(where=".") ===>['atest', 'mypackage', 'mypackage1', 'mypackage.mp0', 'mypackage.my1']
 # 2. find_packages(where=".", exclude=("a*", ) ===> ['mypackage', 'mypackage1', 'mypackage.mp0', 'mypackage.my1']
 # 3. 列表表达式if判断 ===> ['mypackage', 'mypackage1', 'mypackage.mp0']
-# 故:安装的包有mypackage包、mypackage目录下的mp0包、mypackage1包
+# 故:安装的包有mypackage包(不含mp1下内容)、mypackage1包
 packages = [
     package
     for package in find_packages(where=".", # '.'表示当前目录
                                  exclude=("a*", ))
-    if package == package_name or package == package_name + '1' or package.startswith(package_name + ".mp") 
+    if package == 'mypackage' or package == 'mypackage' + '1' or package.startswith('mypackage' + ".mp")
 ] 
 
 setup(
@@ -37,7 +54,8 @@ setup(
 
     # A list of strings specifying the packages that setuptools will manipulate.
     packages=packages,
-    # If set to True, this tells setuptools to automatically include any data files it finds inside your package directories that are specified by your MANIFEST.in file. For more information, see the section on Including Data Files(https://setuptools.pypa.io/en/latest/userguide/quickstart.html#including-data-files).
+    # If set to True, this tells setuptools to automatically include any data files it finds inside your package directories that are specified by your MANIFEST.in file.
+    # For more information, see the section on Including Data Files(https://setuptools.pypa.io/en/latest/userguide/quickstart.html#including-data-files).
     include_package_data=True,
 
     # A string corresponding to a version specifier (as defined in PEP 440) for the Python version, used to specify the Requires-Python defined in PEP 345.
