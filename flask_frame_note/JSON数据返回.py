2a6b8fcd2b5e581a42ded5330243f26fc65f94cf
dcdmm <dcdmmcomeon@gmail.com>
2024-05-10 00:48:29 +0800

    修复一个版本问题

--- flask_frame_note/JSON数据返回.py
+++ flask_frame_note/JSON数据返回.py
@@ -2,17 +2,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 # Serialize objects to ASCII-encoded JSON. If this is disabled, the JSON returned from jsonify will contain Unicode characters.
-app.config['JSON_AS_ASCII'] = False  # 通过设置app.config['JSON_AS_ASCII'] = False,使其正常显示中文
+app.json.ensure_ascii = False  # 通过设置app.json.ensure_ascii = False,使其正常显示中文
 
 
 @app.route('/index')
 def index():
     data = {
-        'name': '张三'
+        'name': '您本次体检血压示:<b>150182 mmHg</b>。'
     }
+
     # Serialize data to JSON and wrap it in a :class:`~flask.Response
     return jsonify(code=200, status=0, message='ok', value=data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
