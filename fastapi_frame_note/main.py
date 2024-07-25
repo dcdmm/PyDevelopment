40137d8c56dc7c0401ebad02302e7b0d540c33c1
dcdmm <dcdmmcomeon@gmail.com>
2024-07-26 04:27:37 +0800

    开启fastapi的学习

---
+++ fastapi_frame_note/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/a")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+import uvicorn
+
+if __name__ == '__main__':
+
+    uvicorn.run(app,  port=8000)
