a64ba58cd8a0041c490b468c81c51468eaf65d69
dcdmm <dcdmmcomeon@gmail.com>
2024-07-31 08:48:45 +0800

    come on

--- fastapi_frame_note/basic.py
+++ fastapi_frame_note/basic.py
@@ -1,26 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
+
 # create a FastAPI "instance"
 app = FastAPI()
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-    tax: float = None
 
+
+# 只接受'GET'请求
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    a = 1 / 0
+    return "<p>Hello, Python!</p>"
 
-@app.api_route("/items/", methods=["GET", "POST"])
-async def read_and_create_items(item: Item = None):
-    if item:
-        # 处理POST请求
-        return {"message": "Item received", "item": item}
-    else:
-        # 处理GET请求
-        return {"message": "This is a GET request, send a POST to see something different"}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8000)
