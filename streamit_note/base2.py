2d3afc8ce34c28807b3855c5537230882a546eec
dcdmm <dcdmmcomeon@gmail.com>
2024-08-22 11:06:36 +0800

    滑滑话

--- streamit_note/base2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
-
-
---
+++ streamit_note/base2_slider.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+a = st.slider(
+    # A short label explaining to the user what this slider is for.
+    label="滑块 a",
+    # The minimum permitted value.
+    min_value=0,
+    # The maximum permitted value.
+    max_value=100,
+    # The value of the slider when it first renders.
+    value=0,
+    # The stepping interval.
+    step=1
+)
+st.write("a value: ", a)
+
+b = st.slider("(范围)滑块 b", 0.0, 100.0, (25.0, 75.0), step=0.01)
+print(type(b))  # print-><class 'tuple'>
+st.write("b value: ", b)
