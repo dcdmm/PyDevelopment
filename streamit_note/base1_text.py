754d8e54f5f1c223cefb4ef566b4611838e70e05
dcdmm <dcdmmcomeon@gmail.com>
2024-08-22 09:52:41 +0800

    text study

--- streamit_note/base1_text.py
+++ streamit_note/base1_text.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-st.title("This is a title")
+st.title("title")
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
-
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
-st.subheader("This is a subheader with a divider", divider="gray")
-st.subheader("These subheaders have rotating dividers", divider=True)
-st.subheader("One", divider=True)
-st.subheader("Two", divider=True)
-st.subheader("Three", divider=True)
-st.subheader("Four", divider=True)
+st.header("header 1")
+st.subheader('subhead 1.1')
+# Shows a colored divider below the header.
+# If True, successive headers will cycle through divider colors.
+# That is, the first header will have a blue line, the second header will have a green line, and so on. If a string, the color can be set to one of the following: blue, green, orange, red, violet, gray/grey, or rainbow.
+st.header("header 2", divider=True)
+st.subheader('subhead 2.1', divider=True)
+st.header("header 3", divider=True)
+st.subheader('subhead 3.1', divider=True)
+st.header("header 4", divider=True)
+st.subheader('subhead 4.1', divider=True)
+st.header("header 5", divider='red')
+st.subheader('subhead 5.1', divider='red')
+st.header("header 6", divider='orange')
+st.subheader('subhead 6.1', divider='orange')
