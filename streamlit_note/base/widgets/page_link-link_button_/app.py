f3d7896a9d984670a58955ab989bf2383c8d0ff1
dcdmm <dcdmmcomeon@gmail.com>
2024-08-23 09:55:49 +0800

    小的完善

--- streamlit_note/base/widgets/page_link-link_button_/app.py
+++ streamlit_note/base/widgets/page_link-link_button_/app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.page_link("pages/matplotlib_f.py",  # 启动脚本文件(app.py)同目录pages目录(目录名必须为pages)下matplotlib_f.py web页面
+st.page_link(page="pages/matplotlib_f.py",  # 启动脚本文件(app.py)同目录pages目录(目录名必须为pages)下matplotlib_f.py web页面
              # The label for the page link. Labels are required for external pages
              label="pages/matplotlib_f.py链接",
              # If icon is None (default), no icon is displayed.
              icon="1️⃣"
              )
-st.page_link("pages/plotly_f.py",  # 启动脚本文件(app.py)同目录pages目录(目录名必须为pages)下plotly_f.py web页面
+st.page_link(page="pages/plotly_f.py",  # 启动脚本文件(app.py)同目录pages目录(目录名必须为pages)下plotly_f.py web页面
              label="pages/plotly_f.py链接", icon="2️⃣")
-st.page_link("http://www.baidu.com",  # URL链接
+st.page_link(page="http://www.baidu.com",  # 外部页面URL链接
              label="百度链接", icon="🌎")
 
 st.link_button(label="URL链接按钮", url="https://www.bilibili.com/")
