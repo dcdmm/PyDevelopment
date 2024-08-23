ad8415fa7cbb43c1fdd68bae38c5cf56182a54ae
dcdmm <dcdmmcomeon@gmail.com>
2024-08-23 10:16:59 +0800

    小的完善

---
+++ streamlit_note/elements/sucess-info-warning.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Display a success message.
+st.success(
+    # The info text to display.
+    body='This is a success message!',
+    # An optional emoji or icon to display next to the alert. If icon is None (default), no icon is displayed.
+    icon="✅"
+)
+
+# Display an informational message.
+st.info('This is a purely informational message', icon="ℹ️")
+
+# Display warning message.
+st.warning('This is a warning', icon="⚠️")
