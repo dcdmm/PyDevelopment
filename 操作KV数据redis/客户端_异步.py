013701089d6ae3f3e12b0a80d3cdd821f28a6157
dcdmm <dcdmmcomeon@gmail.com>
2025-07-04 16:38:05 +0800

    异步over

--- 操作KV数据redis/客户端_异步.py
+++ 操作KV数据redis/客户端_异步.py
@@ -1,10 +1,28 @@
 import redis.asyncio as redis
+import asyncio
 
-
-client = redis.Redis(
+r = redis.Redis(
     host='localhost',
     port=6379,
     password="qwer123456",
     db=0,
     decode_responses=True
 )
+
+
+async def main(client):
+    print(await client.ping())
+
+    print(await client.ping())
+
+    print(await client.exists("china"))
+    print(await client.set('china', 'good'))
+    print(await client.get("china"))
+
+    for key in await client.keys():
+        await client.delete(key)
+    await client.aclose()
+
+
+if __name__ == "__main__":
+    asyncio.run(main(client=r))
