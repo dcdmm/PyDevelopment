84950f2f1b3ef6ada44af1a44748e6851fe6e1b1
dcdmm <dcdmmcomeon@gmail.com>
2026-03-25 11:39:11 +0800

    小幅度优化

--- flask_frame_note/部署gunicorn/main.py
+++ flask_frame_note/部署gunicorn/main.py
@@ -31,7 +31,7 @@ def how_start1():
     """多进程计时"""
     numbers = [30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
     start_f0 = time.time()
-    # 如果gunicorn启动了2个(worker)进程(每个worker进程的线程数为3),每个worker进程运行时都创建了一个ProcessPoolExecutor(max_workers=5),那么总共会启动2*3*5=30个子进程并行该任务(计算斐波拉契数列),可能会导致并行效率下降(CPU超载)
+    # 如果gunicorn启动了2个(worker)进程(每个worker进程的线程数为3),当所有线程同时处理请求时,每个线程都会独立创建一个ProcessPoolExecutor(max_workers=5),那么最多会启动2*3*5=30个子进程并行该任务(计算斐波拉契数列),可能会导致并行效率下降(CPU超载)
     with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
         f0_result = executor.map(fibonacci, numbers)
     end_f0 = time.time()
@@ -45,7 +45,7 @@ async def say_after(delay):
 async def how_start2():
     """异步计时"""
     start_f0 = time.time()
-    # 通过asyncio.run()创建事件循环
+    # 通过asyncio.run()创建事件循环(协程)
     l = await asyncio.gather(say_after(1), say_after(2), say_after(3), say_after(4), say_after(5))
     end_f0 = time.time()
     return '{:.4f} s'.format(end_f0 - start_f0)
@@ -54,13 +54,13 @@ async def how_start2():
 def how_start3():
     start_f0 = time.time()
     # HTTP请求数为2
-    # 
-    # 
-    # # gunicorn设置1(threads = 2 workers = 1): 
+    #
+    #
+    # # gunicorn设置1(threads = 2 workers = 1):
     # # # 每个线程处理一个HTTP请求
     # # # IO密集型耗时5s + HTTP请求1CPU密集型10S + HTTP请求2CPU密集型10s 约等于 25s
-    # 
-    # # # gunicorn设置2(threads = 2 workers = 1): 
+    #
+    # # # gunicorn设置2(threads = 1 workers = 2):
     # # # 每个进程处理一个HTTP请求
     # # # IO密集型耗时5s + CPU密集型10s 约等于 15s
     time.sleep(5)  # IO密集型任务(耗时5s)
