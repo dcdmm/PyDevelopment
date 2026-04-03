e9ca0e1a3ed24f0b45e79b0056cc7ba79541206e
dcdmm <dcdmmcomeon@gmail.com>
2026-04-03 19:50:29 +0800

    文档update

--- fastapi_frame_note/部署_Uvicorn.py
+++ fastapi_frame_note/部署_Uvicorn.py
@@ -7,6 +7,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-#  --host TEXT      Bind socket to this host.  [default: 127.0.0.1]
-#  --port INTEGER       Bind socket to this port. If 0, an available port will be picked.  [default: 8000]
+# --host TEXT            Bind socket to this host.  [default: 127.0.0.1]
+# --port INTEGER         Bind socket to this port. If 0, an available port will be picked.  [default: 8000]
+# --reload                  Enable auto-reload
 # 命令行: `uvicorn 部署_Uvicorn:app --host 0.0.0.0 --port 80`
