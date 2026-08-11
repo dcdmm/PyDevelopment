66af90e44a0818d26606e60403861885df28f20e
dcdmm <dcdmmcomeon@gmail.com>
2026-08-11 18:29:46 +0800

    简单完善

--- pytest_frame_note/tests/test_unittest_mock.py
+++ pytest_frame_note/tests/test_unittest_mock.py
@@ -4,11 +4,19 @@ import pytest
 from core import CalculationService
 
 
+
 def test_01() -> None:
     recorder = Mock()  # 模拟真实的Recorder对象
 
     # 规定调用recorder.record(...)时返回True
-    recorder.record.return_value = True
+    recorder.record.return_value = True 
+
+    # 与上等价
+    # recorder = Mock()
+    # recorder.record = Mock(return_value=True)
+
+    # 与上等价
+    # recorder = Mock(record=Mock(return_value=True))
 
     service = CalculationService(recorder)
     result = service.add_and_record(10, 3)
@@ -29,5 +37,5 @@ def test_02() -> None:
     # match会检查异常消息中是否包含"计算结果记录失败"
     with pytest.raises(RuntimeError, match="计算结果记录失败"):
         service.add_and_record(10, 3)
-        
+
     recorder.record.assert_called_once_with("add", 13)  # 同样进行了一次调用
