a3603d9d5bb24b930f6b03f371e751b87d064b95
dcdmm <dcdmmcomeon@gmail.com>
2026-08-19 11:21:39 +0800

    文档简单修复

--- pytest_frame_note/tests/test_monkeypatch.py
+++ pytest_frame_note/tests/test_monkeypatch.py
@@ -6,10 +6,10 @@ from core import monkeypatch_demo
 
 
 def test_01(monkeypatch: pytest.MonkeyPatch) -> None: 
-    # 调用fake_get_data()时返回"fake data"
+    # 调用fake_get_data(...)时返回"fake data"
     fake_get_data = Mock(return_value="fake data")
 
-    # **暂时**把monkeypatch_demo.get_data替换为fake_get_data
+    # **暂时**把get_data(...)替换为fake_get_data(...)
     monkeypatch.setattr(monkeypatch_demo, "get_data", fake_get_data)
 
     result = monkeypatch_demo.process_data()
@@ -18,11 +18,11 @@ def test_01(monkeypatch: pytest.MonkeyPatch) -> None:
 
     fake_get_data.assert_called_once_with()
 
-    # test_01测试结束后,恢复为monkeypatch_demo.get_data
+    # test_01测试结束后,恢复为get_data(...)
 
 
 def test_02() -> None:
-    # 此时process_data()内部调用是monkeypatch_demo.get_data
+    # 此时process_data()内部调用是get_data(...)
     result = monkeypatch_demo.process_data()
 
     assert result == "processed: real data"
